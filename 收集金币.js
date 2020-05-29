d030050e24887adee1d7349b0b2e1daf1dcd9263
wkmyws <45654909+wkmyws@users.noreply.github.com>
2020-05-30 07:07:21 +0800

    Update 收集金币.js
    增加关闭按钮

--- 收集金币.js
+++ 收集金币.js
@@ -1,8 +1,5 @@
 "auto";
-var str = "";
-str += "屏幕宽度:" + device.width
-str += "\n屏幕高度:" + device.height;
-//toast("屏幕宽度为"+device.width+"   "+"屏幕高度为"+device.height);
+setScreenMetrics(1080, 1920);
 function run() {
     for (var t = 1; true; t++) {
         var emm =find()
@@ -22,25 +19,30 @@ function run() {
 }
 
 function find() {
-    // var t = text("去浏览").findOne(1);
-
     return text("去进店").findOne(1) ||
         text("去浏览").findOne(1) ||
-        //text ("去围观").findOne(1)||
-        //text("去逛逛").findOne(1)||
+        text ("去围观").findOne(1)||
+        text("去逛逛").findOne(1)||
         null;
-
 }
 
-function view() { //15s
+function view() { 
     for (var i = 0; i < 6; i++) {
         swipe(device.width / 2, 4 * device.height / 5, device.width / 2, device.height / 5, 1000);
         sleep(3000)
     }
     back();
 }
-
-
+var w = floaty.window(
+    <frame gravity="center">
+        <button id="btn">关闭脚本</button>
+    </frame>
+);
+w.btn.click(()=>{
+    toast("手动结束运行脚本")
+    w.exitOnClose()
+    w.close()
+ })
 toast("loading")
 sleep(1000)
 run()
